d6a0114f4806c663df72162361355ae78d21b9e6
Julio Cesar <juliocesarteixeira1992@gmail.com>
2021-02-08 14:13:19 +0100

    js initial template

--- index.js
+++ index.js
@@ -25,5 +25,28 @@ console.log(ditto);
 
 
 
+
+
+
+
 //Julio territory - input
 
+async function pokemonInfo()  {
+    const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/' + input)
+    return await pokemon.json();
+}
+console.log(pokemonInfo());
+
+document.getElementById('run').addEventListener('click', () =>{
+    
+
+    pokemonInfo().then(resolve =>{ //async function to display pokemons() ?
+        let pokemon = resolve.pokemons.find(pokemon => pokemon.id === input);
+        let pokemonResult = template.cloneNode(true);
+            pokemonResult.querySelector('.name').innerHTML = pokemon.name;
+            // pokemonResult.querySelector('.img-pokemon').innerHTML = '<img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}" />';
+            // pokemonResult.querySelector('.moves').innerText = pokemon.abilities.name;
+
+            target.append(pokemonResult);
+    }, reject => console.error(reject('did not work')));
+})
