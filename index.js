83f2c451c0c9c0c6ebfc6914c606473ed1fa19fb
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-02-08 14:31:30 +0100

    merged fetch functions and eventlistener

--- index.js
+++ index.js
@@ -29,44 +29,39 @@ async function getPokemonImg(id) {
 })();
 
 
+// async function pokemonInfo()  {
+//     const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/' + input)
+//     return await pokemon.json();
+// }
+// console.log(pokemonInfo());
 
+document.getElementById('run').addEventListener('click', async () => {
 
+    let pokemon = await getPokemon(input);
 
+    let id = pokemon.id;
+    let name = pokemon.name;
 
+    let img = await getPokemonImg(id);
 
+    let target = document.getElementById("target");
+    let template = document.getElementById("tpl-pokemon").content;
+    let pokemonResult = template.cloneNode(true);
 
+    pokemonResult.querySelector('.name').innerHTML = name;
+    pokemonResult.querySelector('.img-pokemon').innerHTML = img;
+    pokemonResult.querySelector(".ID-number").innerHTML = id;
+    // pokemonResult.querySelector('.moves').innerText = pokemon.abilities.name;
 
+    // Marte's evolution function
 
-
-
-
-
-
-
-
-
-
-
-
-
-//Julio territory - input
-
-async function pokemonInfo()  {
-    const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/' + input)
-    return await pokemon.json();
-}
-console.log(pokemonInfo());
-
-document.getElementById('run').addEventListener('click', () =>{
     
 
-    pokemonInfo().then(resolve =>{ //async function to display pokemons() ?
-        let pokemon = resolve.pokemons.find(pokemon => pokemon.id === input);
-        let pokemonResult = template.cloneNode(true);
-            pokemonResult.querySelector('.name').innerHTML = pokemon.name;
-            // pokemonResult.querySelector('.img-pokemon').innerHTML = '<img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}" />';
-            // pokemonResult.querySelector('.moves').innerText = pokemon.abilities.name;
 
-            target.append(pokemonResult);
-    }, reject => console.error(reject('did not work')));
+    // Julio's moves function
+
+
+
+
+    target.append(pokemonResult);
 })
