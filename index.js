f5c154dd35a16f45413618ff4fd7fc554755a9b9
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-02-08 14:09:48 +0100

    added getPokemonImg function

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 // fetch function
 
-let ditto = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then(response => {
-        return response.json()
-    })
-    .then(pokemon => {console.log(pokemon)});
+async function getPokemon(input) {
+    let pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + input);
+    return await pokemon.json();
+}
 
-console.log(ditto);
+async function getPokemonImg(id) {
+    let img = await fetch("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png");
+    let imgBlob = await img.blob()
+    return URL.createObjectURL(imgBlob);
+}
+
+(async () => {
+
+    let input = "18";
+    let pokemon = await getPokemon(input);
+
+    let id = pokemon.id;
+    let name = pokemon.name;
+
+    let img = await getPokemonImg(id);
+
+    console.log(id, name);
+    console.log(img);
+
+    document.getElementById("pokemon-id").src = img;
+
+})();
 
 
 
