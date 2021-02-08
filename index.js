7494b981b982943cab79d7e6f028d4cd75b9da49
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-02-08 10:43:32 +0100

    added fetch function

---
+++ index.js
@@ -0,0 +1,9 @@
+// fetch function
+
+let ditto = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then(response => {
+        return response.json()
+    })
+    .then(pokemon => {console.log(pokemon)});
+
+console.log(ditto);
